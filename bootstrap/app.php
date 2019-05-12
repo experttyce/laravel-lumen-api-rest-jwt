9681f9c55edd4bb387956a52397cc299f1acf30a
Carlos Orozco <carlos.orozco@tigears.com>
2019-05-12 13:53:08 +0000

    incluye generadores

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -94,6 +94,7 @@ $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 // Add this line
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
